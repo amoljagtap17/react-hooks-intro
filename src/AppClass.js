053e93f20f2823d993940efa60f4eec02f53539d
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-02-16 13:38:45 +0530

    Working with multiple states

--- src/AppClass.js
+++ src/AppClass.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react'
 
 class App extends Component {
   state = {
-    count: 0
+    count: 0,
+    isOn: false
   }
 
   incrementCount = () => {
     this.setState(prevState => ({ count: prevState.count + 1 }))
   }
 
+  toggleColor = () => {
+    this.setState(prevState => ({ isOn: !prevState.isOn }))
+  }
+
   render() {
-    const { count } = this.state
+    const { count, isOn } = this.state
 
     return (
-      <button onClick={this.incrementCount}>I was clicked {count} times</button>
+      <>
+        <h2>Counter</h2>
+        <button onClick={this.incrementCount}>
+          I was clicked {count} times
+        </button>
+
+        <h2>Toggle Color</h2>
+        <div
+          style={{
+            width: '100px',
+            height: '100px',
+            background: isOn ? 'grey' : 'yellow'
+          }}
+          onClick={this.toggleColor}
+        />
+      </>
     )
   }
 }
--- src/AppFunc.js
+++ src/AppFunc.js
@@ -2,12 +2,37 @@ import React, { useState } from 'react'
 
 const AppFunc = () => {
   const [count, setCount] = useState(0)
+  const [isOn, setIsOn] = useState(false)
 
   const incrementCount = () => {
     setCount(prevCount => prevCount + 1)
   }
 
-  return <button onClick={incrementCount}>I was clicked {count} times</button>
+  const toggleColor = () => {
+    setIsOn(prevIsOn => !prevIsOn)
+  }
+
+  return (
+    <>
+      <h2>Counter</h2>
+      <button onClick={incrementCount}>I was clicked {count} times</button>
+
+      <h2>Toggle Color</h2>
+      <img
+        src={
+          isOn
+            ? 'https://icon.now.sh/highlight/fd0'
+            : 'https://icon.now.sh/highlight/aaa'
+        }
+        style={{
+          width: '100px',
+          height: '100px'
+        }}
+        alt="FlashLight"
+        onClick={toggleColor}
+      />
+    </>
+  )
 }
 
 export default AppFunc
