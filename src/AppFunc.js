66554b551e77387c9db4ee3a27009cf4e6f2d282
Amol Jagtap <ajfsdeveloper@gmail.com>
2019-02-16 20:04:49 +0530

    Cleaning listeners without a supportive api

--- src/AppFunc.js
+++ src/AppFunc.js
@@ -1,26 +1,52 @@
 import React, { useState, useEffect } from 'react'
 
+const initialLocationState = {
+  latitude: null,
+  longitude: null,
+  speed: null
+}
+
 const AppFunc = () => {
   const [count, setCount] = useState(0)
   const [isOn, setIsOn] = useState(false)
   const [mousePosition, setMousePosition] = useState({ x: null, y: null })
   const [status, setStatus] = useState(navigator.onLine)
+  // const [location, setLocation] = useState(initialLocationState)
+  const [{ latitude, longitude, speed }, setLocation] = useState(
+    initialLocationState
+  )
+
+  let mounted = true
 
   useEffect(() => {
     document.title = `You have clicked ${count} times!`
     window.addEventListener('mousemove', handleMouseMove)
     window.addEventListener('online', handleOnline)
     window.addEventListener('offline', handleOffline)
+    navigator.geolocation.getCurrentPosition(handleGeolocation)
+    const watchId = navigator.geolocation.watchPosition(handleGeolocation)
 
     return () => {
       window.removeEventListener('mousemove', handleMouseMove)
       window.removeEventListener('online', handleOnline)
       window.removeEventListener('offline', handleOffline)
+      navigator.geolocation.clearWatch(watchId)
+      mounted = false
     }
   }, [count])
 
   // Above empty [] runs only on componentDidMount and componentWillUnmount
   // If we specify something like count, any changes to this will re-render
+
+  const handleGeolocation = event => {
+    if (mounted) {
+      setLocation({
+        latitude: event.coords.latitude,
+        longitude: event.coords.longitude,
+        speed: event.coords.speed
+      })
+    }
+  }
 
   const handleOnline = () => {
     setStatus(true)
@@ -73,6 +99,15 @@ const AppFunc = () => {
       <p>
         You are <strong>{status ? 'online' : 'offline'}</strong>
       </p>
+
+      <h2>Geolocation</h2>
+      {/* <p>Latitude is {location.latitude}</p>
+      <p>Longitude is {location.longitude}</p>
+      <p>Speed is {location.speed ? location.speed : '0'}</p> */}
+
+      <p>Latitude is {latitude}</p>
+      <p>Longitude is {longitude}</p>
+      <p>Speed is {speed ? speed : '0'}</p>
     </>
   )
 }
